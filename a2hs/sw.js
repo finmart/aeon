2c2c7ab8bea6355575ca64a04f10c817ed5e93a0
FINMART <71874851+finmart@users.noreply.github.com>
2020-11-11 17:12:13 +0700

    Update sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('fox-store').then(function(cache) {
      return cache.addAll([
-       '/pwa-examples/a2hs/',
-       '/pwa-examples/a2hs/index.html',
-       '/pwa-examples/a2hs/index.js',
-       '/pwa-examples/a2hs/style.css',
-       '/pwa-examples/a2hs/images/fox1.jpg',
-       '/pwa-examples/a2hs/images/fox2.jpg',
-       '/pwa-examples/a2hs/images/fox3.jpg',
-       '/pwa-examples/a2hs/images/fox4.jpg'
+       '/aeon/a2hs/',
+       '/aeon/a2hs/index.html',
+       '/aeon/a2hs/index.js',
+       '/aeon/a2hs/style.css',
+       '/aeon/a2hs/images/fox1.jpg',
+       '/aeon/a2hs/images/fox2.jpg',
+       '/aeon/a2hs/images/fox3.jpg',
+       '/aeon/a2hs/images/fox4.jpg'
      ]);
    })
  );
